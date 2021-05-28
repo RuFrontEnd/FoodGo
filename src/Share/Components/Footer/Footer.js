d04f59e505f89641ed70e3ce0d7efe5465508e37
RuFrontEnd <k718198@gmail.com>
2021-05-28 19:55:55 +0800

    create footer rwd

--- src/Share/Components/Footer/Footer.js
+++ src/Share/Components/Footer/Footer.js
@@ -4,34 +4,42 @@ import Facebook from '../Images/SVG/facebook.svg';
 import Instagram from '../Images/SVG/instagram.svg';
 
 function Footer() {
-    return (
-        <>
-            <div className="footer-background">
-                <div className="footer-card">
-                    <div className="footer-text-box">
-                        <div className="footer-text">
-                            <a href="#"><p>常見問題</p></a>
-                            <a href="#"><p>服務條款</p></a>
-                            <a href="#"><p>關於我們</p></a>
-                            <a href="#"><p>隱私權政策</p></a>
-                        </div>
-                    </div>
-                    <div className="footer-icons">
-                        <a>
-                            <img src={Facebook} />
-                        </a>
-                        <a>
-                            <img src={Instagram} />
-                        </a>
-                    </div>
-                    <div className="footer-copyright">
-                        <p>Copyright © 2020 拾餐便當. All rights reserved.</p>
-                    </div>
-                </div>
+  return (
+    <>
+      <div style={{paddingTop:"150px"}}></div>
+      <section className="footer-container">
+        <div className="footer-wrap">
+          <div className="footer-text-box">
+            <div className="footer-text">
+              <a href="#">
+                <p>常見問題</p>
+              </a>
+              <a href="#">
+                <p>服務條款</p>
+              </a>
+              <a href="#">
+                <p>關於我們</p>
+              </a>
+              <a href="#">
+                <p>隱私權政策</p>
+              </a>
             </div>
-        </>
-    )
-
+          </div>
+          <div className="footer-icons">
+            <a>
+              <img src={Facebook} />
+            </a>
+            <a>
+              <img src={Instagram} />
+            </a>
+          </div>
+          <div className="footer-copyright">
+            <p>Copyright © 2020 拾餐便當. All rights reserved.</p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default Footer;
