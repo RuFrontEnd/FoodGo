9cee41c3121b4c77c05a40f98bbe8b5d1734b9ee
RuFrontEnd <k718198@gmail.com>
2021-06-03 08:54:22 +0800

    put introCard viewMore btn position

--- src/components/introCard/IntroCard.js
+++ src/components/introCard/IntroCard.js
@@ -15,18 +15,20 @@ function IntroCard(props) {
     <>
       <section id="introCard-container" className={className} style={style}>
         <div id="introCard-wrap">
+          <img id="introCard-photo" src={photo}></img>
           <div id="introCard-photo-fake"></div>
           <div id="introCard-content">
             <p id="introCard-title">{title}</p>
             <p id="introCard-text">{content}</p>
-            <Link to={linkTo}>
+          </div>
+          <Link to={linkTo}>
+            <span id="introCard-btn">
               <div id="introCard-arrow"></div>
               <br />
               <br />
               <p id="introCard-viewMore">view more</p>
-            </Link>
-          </div>
-          <img id="introCard-photo" src={photo}></img>
+            </span>
+          </Link>
         </div>
       </section>
     </>
