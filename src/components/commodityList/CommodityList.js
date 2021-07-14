22c706b3d7801157e3a740888c221c40f72f0089
RuFrontEnd <k718198@gmail.com>
2021-07-14 20:03:11 +0800

    clean code

--- src/components/commodityList/CommodityList.js
+++ src/components/commodityList/CommodityList.js
@@ -14,6 +14,27 @@ function CommodityList(props) {
     setCount,
   } = props;
 
+  // const [showFavArr, setShowFavArr] = useState([]);
+
+  // useEffect(() => {
+  //   // 等待兩個fetch都結束
+  //   if (!data || !dataFav) {
+  //     return;
+  //   }
+
+  //   // 拿到有幾筆要固定我的最愛按鈕 邏輯
+  //   const favArr = []; // 放抓到的dataFav[i].product_sid資料
+  //   for (let i = 0; i < dataFav.length; i++) {
+  //     // 如果當前會員 跟 我的最愛資料表的member_sid匹配
+  //     if (currentUser === dataFav[i].member_sid) {
+  //       // console.log(dataFav[i].product_sid)
+  //       favArr.push(dataFav[i].product_sid);
+  //     }
+  //   }
+  //   // console.log(favArr)
+  //   setShowFavArr(favArr); // 這樣才可以傳到RuAddFavorite
+  // }, [searchInput, data, dataFav]); // 如果這邊沒有設定state, 就只會在掛載時執行一次 / 如果有, 在每次state變動時都會執行一次.
+
   useEffect(() => {
     if (!commodities || !favorites) {
       return;
@@ -44,10 +65,7 @@ function CommodityList(props) {
             {/* {isShowNothing && <EmptyHint />} */}
             {commodities.map((commodity) => (
               <ProductCard
-<<<<<<< HEAD
                 id={`ru-addCart-btn-${commodity.sid}`}
-=======
->>>>>>> parent of 7f5406d... 去除productCard不必要參數
                 productSid={commodity.sid}
                 favorites={favorites}
                 title={commodity.productname}
@@ -55,7 +73,6 @@ function CommodityList(props) {
                 buy={commodity.purchased}
                 price={commodity.price}
                 stars={commodity.startRating}
-                id={`ru-addCart-btn-${index + 1}`}
                 proudctId={commodity.sid}
                 parentId={`ru-addCart-btn-warp-${index + 1}`}
                 imgId={commodity.img_id}
