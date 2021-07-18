644778a2847add82a77f41797765881f5a69df01
RuFrontEnd <k718198@gmail.com>
2021-07-18 18:45:12 +0800

    修改沙拉區資料與樣式

--- src/pages/saladList/SaladList.js
+++ src/pages/saladList/SaladList.js
@@ -48,7 +48,7 @@ function SaladList(props) {
     axios.get(`${endpoint}/product/bento`).then((res) => {
       console.log('res', res);
       const _commodities = res.data.filter(
-        (dataItem) => dataItem.categories === '1.低GI便當'
+        (dataItem) => dataItem.categories === '2.蔬食沙拉'
       );
       console.log('_commodities', _commodities);
       setCommodities(_commodities);
