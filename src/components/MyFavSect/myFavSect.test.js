a6c8dbd91a71c4a79c59e6f2e588a78a456e9c70
RuFrontEnd <k718198@gmail.com>
2021-07-24 21:34:16 +0800

    test myFav api

--- src/components/MyFavSect/myFavSect.test.js
+++ src/components/MyFavSect/myFavSect.test.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import { render, screen } from '@testing-library/react';
 import MyFavSect, { getMyFav } from 'components/myFavSect/MyFavSect';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from 'redux/store';
+import axios from 'axios';
 
 describe('測試myFavSect.js', () => {
   test('是否取得我的最愛資料', async () => {
@@ -11,12 +13,19 @@ describe('測試myFavSect.js', () => {
   });
 
   test('是否myFavSect元件成功渲染', async () => {
+    // const productModule = {
+    //   getMyFavFn: getMyFav,
+    // };
     render(
       <Provider store={store}>
-        <MyFavSect />
+        <Router>
+          <MyFavSect />
+        </Router>
       </Provider>
     );
+
     await getMyFav(Math.random()).then((res) => {
+      console.log('res.data', res.data);
       const title = screen.getByTestId('myFavSect-title');
       expect(title).toBeInTheDocument();
     });
