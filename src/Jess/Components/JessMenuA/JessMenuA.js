4bc063094dd00dd89670601d36843c981f08e853
RuFrontEnd <k718198@gmail.com>
2021-07-29 20:29:41 +0800

    新增styled component props

--- src/Jess/Components/JessMenuA/JessMenuA.js
+++ src/Jess/Components/JessMenuA/JessMenuA.js
@@ -5,39 +5,40 @@ import TweenOne from 'rc-tween-one';
 import 'rc-banner-anim/assets/index.css';
 import styled from 'styled-components';
 
-const MenuContainer = styled.div`
-  background-color: $fff;
-  width: 100%;
-  height: 55rem;
-  padding: 0;
-`;
-
-function JessMenuA() {
+function JessMenuA(props) {
+  const {className, id, style} = props
+  const MenuContainer = styled.div`
+    background-color: $fff;
+    width: 100%;
+    height: 55rem;
+    padding: 0;
+    ${style && style}
+  `;
   const BgElement = Element.BgElement;
   const Demo = () => {
     return (
       <>
         <MenuContainer>
           {/* <div className="jess-MenuA-container-fluid"> */}
-            <div className="container-fluid jess-container">
-              <BannerAnim prefixCls="jess-MenuBanner" autoPlay>
-                <Element prefixCls="jess-MenuBanner-elem" key="0">
-                  <BgElement key="bg" className="bg" />
-                  <TweenOne
-                    className="jess-MenuBanner-title"
-                    animation={{ x: -30, opacity: 0, type: 'from' }}
-                  >
-                    生活不將就
-                  </TweenOne>
-                  <TweenOne
-                    className="jess-MenuBanner-text"
-                    animation={{ y: 40, opacity: 0, type: 'from', delay: 700 }}
-                  >
-                    吃飯就該 好好講究
-                  </TweenOne>
-                </Element>
-              </BannerAnim>
-            </div>
+          <div className="container-fluid jess-container">
+            <BannerAnim prefixCls="jess-MenuBanner" autoPlay>
+              <Element prefixCls="jess-MenuBanner-elem" key="0">
+                <BgElement key="bg" className="bg" />
+                <TweenOne
+                  className="jess-MenuBanner-title"
+                  animation={{ x: -30, opacity: 0, type: 'from' }}
+                >
+                  生活不將就
+                </TweenOne>
+                <TweenOne
+                  className="jess-MenuBanner-text"
+                  animation={{ y: 40, opacity: 0, type: 'from', delay: 700 }}
+                >
+                  吃飯就該 好好講究
+                </TweenOne>
+              </Element>
+            </BannerAnim>
+          </div>
           {/* </div> */}
         </MenuContainer>
       </>
