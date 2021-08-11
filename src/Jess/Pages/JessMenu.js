0c544e565208a2276f8378965f14930803a50aef
RuFrontEnd <k718198@gmail.com>
2021-08-11 20:25:39 +0800

    DailyMenu 製作活力蔬菜區

--- src/Jess/Pages/JessMenu.js
+++ src/Jess/Pages/JessMenu.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { withRouter, useParams } from 'react-router-dom';
 import MenuBanner from 'components/menuBanner/MenuBanner';
 import DailyMenu from 'components/dailyMenu/DailyMenu';
-import JessMenuC from '../Components/JessMenuC/JessMenuC';
+// import JessMenuC from '../Components/JessMenuC/JessMenuC';。
 import JessMenuD from '../Components/JessMenuD/JessMenuD';
 import ToTop from 'Share/Components/ToTopButton/ScrollButton';
 import styled from 'styled-components/macro';
@@ -11,6 +11,7 @@ import bentoChickenBreast from 'assets/jpg/00_bento-chicken-breast.jpg';
 import bentoChickenThigh from 'assets/jpg/01_bento-chicken-thigh.jpg';
 import bentoTenderLoin from 'assets/jpg/06_bento-tenderloin.jpg';
 import bentoShrimp from 'assets/jpg/03_bento-shrimp.jpg';
+import menuVegetableBox from 'assets/jpg/menu-vegetableBox.jpg';
 
 function JessMenu(props) {
   const { setShowBar } = props;
@@ -66,7 +67,44 @@ function JessMenu(props) {
         }}
         bottomLink={{ linkTo: '/productList', text: '低GI便當' }}
       />
-      <JessMenuC id={id} />
+      <DailyMenu
+        title={'活力蔬菜'}
+        isMainItemLeft={false}
+        mainText={'從產地到餐桌，最新鮮、美味、安心的來源'}
+        viceText={
+          ' “當季、新鮮、農場直送”的好食材嚴選的品質和穩定的貨源，我們才能吃的放心、有保障。'
+        }
+        mainItem={{
+          title: '活力蔬菜',
+          photo: menuVegetableBox,
+          linkTo: '/productList',
+        }}
+        firstSubItem={{
+          title: '中歐香料嫩雞胸',
+          price: 170,
+          photo: bentoChickenBreast,
+          linkTo: '/bento/0',
+        }}
+        secondSubItem={{
+          title: '日式燒雞腿',
+          price: 150,
+          photo: bentoChickenThigh,
+          linkTo: '/bento/1',
+        }}
+        thirdSubItem={{
+          title: '頂級熟成菲力牛排',
+          price: 230,
+          photo: bentoTenderLoin,
+          linkTo: '/bento/6',
+        }}
+        fourthSubItem={{
+          title: '熱帶火烤萊姆蝦',
+          price: 200,
+          photo: bentoShrimp,
+          linkTo: '/bento/3',
+        }}
+        bottomLink={{ linkTo: '/productList', text: '低GI便當' }}
+      />
       <JessMenuD />
       <ToTop />
     </>
