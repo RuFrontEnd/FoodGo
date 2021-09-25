ae87dbf27c265fd05f9fd754bc06db7de8fce959
RuFrontEnd <k718198@gmail.com>
2021-09-25 12:57:27 +0800

    create farmCardIntro components

--- src/Claudia/Components/ClaudiaIndexContent/ClaudiaIndexContent.js
+++ src/Claudia/Components/ClaudiaIndexContent/ClaudiaIndexContent.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import './ClaudiaIndexContent.scss';
 import FarmMap from 'components/farmMap/FarmMap';
-import ClaudiaIndexCardIntro from '../ClaudiaIndexCardIntro/ClaudiaIndexCardIntro';
-import ClaudiaIndexCardList from '../ClaudiaIndexCardList/ClaudiaIndexCardList';
+import FarmCard from 'components/farmCard/FarmCard';
+import FarmCardIntro from 'components/farmCardIntro/FarmCardIntro';
 
 function ClaudiaIndexContent() {
   const [city, setCity] = useState('');
@@ -16,26 +16,21 @@ function ClaudiaIndexContent() {
         <div className="claudia-index-content-fix-container">
           <div className="claudia-index-content-container">
             <div className="claudia-index-map">
-              <FarmMap
-                setCity={setCity}
-                setData={setData}
-                data={data}
-              />
+              <FarmMap setCity={setCity} setData={setData} data={data} />
             </div>
-            <div className="claudia-index-card">
-              <ClaudiaIndexCardList
-                city={city}
-                data={data}
-                setButtonNum={setButtonNum}
-                setImgNum={setImgNum}
-              />
-              <ClaudiaIndexCardIntro
+            <FarmCard
+              city={city}
+              data={data}
+              setButtonNum={setButtonNum}
+              setImgNum={setImgNum}
+            >
+              <FarmCardIntro
                 city={city}
                 data={data}
                 imgNum={imgNum}
                 buttonNum={buttonNum}
               />
-            </div>
+            </FarmCard>
           </div>
         </div>
       </div>
