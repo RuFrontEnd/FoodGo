061420b76f315909be25bdd5e7779ecb3ffe1474
Claudia Teng <sydeng102@gmail.com>
2021-10-11 20:12:17 +0800

    remove direact dom manipulation for image slider

--- src/components/imageSlider/ImageSlider.js
+++ src/components/imageSlider/ImageSlider.js
@@ -8,74 +8,35 @@ function ImageSlider(props) {
   const imageSlider = (e) => {
     let selectImg = e.target.src;
     setImgSrc(selectImg);
-
-    // change triangle display - step 1: find index - make triangle appears
-    const child = e.target;
-    const parent = child.parentNode;
-    const index = Array.prototype.indexOf.call(parent.children, child);
-    const triangleIcon =
-      e.target.parentNode.nextElementSibling.children[index].children[0];
-    triangleIcon.style.visibility = 'visible';
-
-    // change triangle display - step 2: find .siblings - make triangles disappear
-    const getSiblings = function (e) {
-      let siblings = [];
-
-      if (!e.parentNode) {
-        return siblings;
-      }
-
-      let sibling = e.parentNode.firstChild;
-
-      while (sibling) {
-        if (sibling.nodeType === 1 && sibling !== e) {
-          siblings.push(sibling);
-        }
-        sibling = sibling.nextSibling;
-      }
-      return siblings;
-    };
-
-    let siblings = getSiblings(triangleIcon.parentNode);
-
-    siblings.forEach((el) => {
-      const siblingsTriangles = el.children[0];
-      // console.log('loop', el.children[0]);
-      console.log('siblingsTriangles', siblingsTriangles);
-      siblingsTriangles.style.visibility = 'hidden';
-    });
   };
 
   return (
     <>
       <div className="claudia-detailed-slider-container">
         <div className="claudia-image-select">
-          <img onClick={imageSlider} alt="" src={pic1} />
-          <img onClick={imageSlider} alt="" src={pic2} />
-          <img onClick={imageSlider} alt="" src={pic3} />
-          <img onClick={imageSlider} alt="" src={pic4} />
-        </div>
-        <div className="claudia-image-triangle-area">
-          <div className="claudia-image-triangle-box">
-            <div className="claudia-image-triangle"></div>
+          <div>
+            <img onClick={imageSlider} alt="" src={pic1} />
+            <div className="claudia-image-triangle-box">
+            { imgSrc.includes(pic1) && <div className="claudia-image-triangle"></div> }
+            </div>
           </div>
-          <div className="claudia-image-triangle-box">
-            <div
-              style={{ visibility: 'hidden' }}
-              className="claudia-image-triangle"
-            ></div>
+          <div>
+            <img onClick={imageSlider} alt="" src={pic2} />
+            <div className="claudia-image-triangle-box">
+            { imgSrc.includes(pic2) && <div className="claudia-image-triangle"></div> }
+            </div>
           </div>
-          <div className="claudia-image-triangle-box">
-            <div
-              style={{ visibility: 'hidden' }}
-              className="claudia-image-triangle"
-            ></div>
+          <div>
+            <img onClick={imageSlider} alt="" src={pic3} />
+            <div className="claudia-image-triangle-box">
+            { imgSrc.includes(pic3) && <div className="claudia-image-triangle"></div> }
+            </div>
           </div>
-          <div className="claudia-image-triangle-box">
-            <div
-              style={{ visibility: 'hidden' }}
-              className="claudia-image-triangle"
-            ></div>
+          <div>
+            <img onClick={imageSlider} alt="" src={pic4} />
+            <div className="claudia-image-triangle-box">
+            { imgSrc.includes(pic4) && <div className="claudia-image-triangle"></div> }
+            </div>
           </div>
         </div>
         <div className="claudia-image-large">
